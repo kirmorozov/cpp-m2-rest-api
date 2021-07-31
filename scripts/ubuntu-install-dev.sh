df1d0ceb2d079e3cc3578def4d6482972ee0b6c6
Kirill Morozov <kir.morozov@gmail.com>
2021-07-31 16:45:44 -0700

    Using json configuration file.

--- scripts/ubuntu-install-dev.sh
+++ scripts/ubuntu-install-dev.sh
@@ -4,4 +4,4 @@ wget https://dev.mysql.com/get/mysql-apt-config_0.8.18-1_all.deb &&
 sudo dpkg -i mysql-apt-config_0.8.18-1_all.deb &&
 sudo apt update &&
 sudo apt install libpistache-dev libmysqlcppconn-dev libsodium-dev &&
-wget -O 3rdparty/includes/json.hpp https://github.com/nlohmann/json/releases/download/v3.9.1/json.hpp
+wget -O ../3rdparty/includes/json.hpp https://github.com/nlohmann/json/releases/download/v3.9.1/json.hpp
