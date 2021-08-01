3c74558d641f508e0270d108121f32b7b1ed1408
Kirill Morozov <kir.morozov@gmail.com>
2021-07-31 21:12:18 -0700

    Installing and waiting for mysql.

--- scripts/mysql-prepare.sh
+++ scripts/mysql-prepare.sh
@@ -7,8 +7,8 @@ sudo service mysql stop
 
 sudo apt install mysql-server
 sudo service mysql start
-sleep 14
-sudo service mysql status
+mysqladmin ping -u root -proot -h 127.0.0.1 --wait=90
 
+mysqladmin -u root -proot -h 127.0.0.1 password '123123qa'
 
-echo "create database db;" | mysql -u root -h 127.0.0.1
+echo "create database db;" | mysql -u root -p'123123qa' -h 127.0.0.1
