5daae9396f85a618e13c85062d56a1ae3ff25459
Kirill Morozov <kir.morozov@gmail.com>
2022-01-24 05:56:45 +0300

    Update mysql dependencies

--- scripts/ubuntu-install-dev.sh
+++ scripts/ubuntu-install-dev.sh
@@ -2,7 +2,7 @@
 export DEBIAN_FRONTEND=noninteractive
 sudo add-apt-repository ppa:pistache+team/unstable
 wget https://dev.mysql.com/get/mysql-apt-config_0.8.22-1_all.deb
-sudo dpkg -i mysql-apt-config_0.8.18-1_all.deb
+sudo dpkg -i mysql-apt-config_0.8.22-1_all.deb
 sudo apt update
 sudo apt install libpistache-dev libmysqlcppconn-dev libsodium-dev libhiredis-dev redis-server libmysqlcppconn8-1
 sudo service redis-server start
